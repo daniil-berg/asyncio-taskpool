cbb3ae4989170540b6668a8dc45be14f75a8b787
Daniil Fajnberg <mail@daniil.fajnberg.de>
2023-10-10 20:46:31 +0200

    🔨 Move `cloc.sh` to `scripts` directory

--- scripts/cloc.sh
+++ scripts/cloc.sh
@@ -1,17 +1,5 @@
 #!/usr/bin/env bash
 
-# This file is part of asyncio-taskpool.
-
-# asyncio-taskpool is free software: you can redistribute it and/or modify it under the terms of
-# version 3.0 of the GNU Lesser General Public License as published by the Free Software Foundation.
-
-# asyncio-taskpool is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the GNU Lesser General Public License for more details.
-
-# You should have received a copy of the GNU Lesser General Public License along with asyncio-taskpool.
-# If not, see <https://www.gnu.org/licenses/>.
-
 typeset option
 if getopts 'bcmp' option; then
   if [[ ${option} == [bcmp] ]]; then
